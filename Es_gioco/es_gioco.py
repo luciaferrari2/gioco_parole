6a9c7b4917ae22a95d5ada3fce53e75652e3cf47
luciaferrari2 <139228762+luciaferrari2@users.noreply.github.com>
2023-07-11 14:48:44 +0200

    Traccia gioco e nomi variabili

--- Es_gioco/es_gioco.py
+++ Es_gioco/es_gioco.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+## GIOCO DELL'IMPICCATO ##
+
+#1) Richiesta: "Vuoi giocare?" ----> accesso
+#2) Abbiamo una lista di parole ----> lista_parole
+#3) Numero tentativi sbagliati ----> tentativi_sbagliati
+#4) Stringa che prende randomicamente una parola da lista_parole ----> parola
+#5) Input dell'utente ----> lettera
+#6) Due condizioni per uscire dai cicli for/while ----> tentativi finiti o parola trovata
+#7) Inizializzare una stringa composta da "_" di lunghezza = len(parola) ----> parola_segreta
+
+
